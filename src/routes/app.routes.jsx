23ca5d0d3399541d21d2fec7af2f9f842d68c175
caickdias <caickandrade0@gmail.com>
2022-12-16 00:33:24 -0300

    add: react navigation and home screen

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from '../screens/Home';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export const AppRoutes = () => {
+    return(
+        <Navigator
+            screenOptions={{ headerShown: false }}
+            initialRouteName="Home"
+        >
+            <Screen 
+                name="Home"
+                component={Home}
+            />
+        </Navigator>
+    )
+}
+
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+import { styles } from './styles';
+
+const Home = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Adicionar nova peladaaa</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
